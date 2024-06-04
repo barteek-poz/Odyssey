12d752b6f70279d2484999da765353a034918aab
barteek-poz <szowior4@wp.pl>
2024-06-04 15:40:44 +0200

    dynamic change of transport type img

--- src/pages/Travels.jsx
+++ src/pages/Travels.jsx
@@ -1,32 +1,35 @@
 import Map from "../components/Map";
 import Navigation from "../components/Navigation";
 import TravelBig from "../components/TravelBig";
-import {useLoaderData} from 'react-router-dom'
+import { useLoaderData } from "react-router-dom";
 
 const Travels = () => {
-  const loaderTravels = useLoaderData()
+  const loaderTravels = useLoaderData();
 
   return (
     <section className="TRAVELS w-full h-full">
       <Navigation />
       <div className="MAP-CONTAINER w-full h-96">
-        <Map travels={loaderTravels}/>
+        <Map travels={loaderTravels} />
       </div>
       <h1 className="sm:text-xl uppercase p-8 ml-8">Your travels</h1>
       <div className="TRAVELS-LIST mx-16 flex flex-wrap justify-between gap-8 ">
-        {loaderTravels?.map(travel => {
-          return  <TravelBig
-          key={travel.id}
-          id={travel.id}
-          img={travel.img}
-          location={travel.location}
-          date={travel.date}
-          from={travel.transport?.to.cityDeparture}
-          to={travel.transport?.from.cityArrival}
-          accomodation={travel.accomodation?.place}
-        />
+        {loaderTravels?.map((travel) => {
+          return (
+            <TravelBig
+              key={travel.id}
+              id={travel.id}
+              img={travel.img}
+              location={travel.location}
+              date={travel.date}
+              from={travel.transport?.to.cityDeparture}
+              to={travel.transport?.from.cityArrival}
+              transportTo={travel.transport?.to.type}
+              transportFrom={travel.transport?.from.type}
+              accomodation={travel.accomodation?.place}
+            />
+          );
         })}
-        
       </div>
     </section>
   );
