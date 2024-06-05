0aec436cdb678d2a872da30707b8b37ffd57ae35
barteek-poz <szowior4@wp.pl>
2024-06-05 20:39:26 +0200

    changed time input to type time

--- src/components/TransportForm.jsx
+++ src/components/TransportForm.jsx
@@ -61,7 +61,7 @@ const transportInputHandler = (category, inputValue) => {
         label="Time of departure"
         placeholder="Enter your time of departure"
         category="timeDeparture"
-        
+        type="time"
         updateDataHandler={transportInputHandler}
       />
       <DetailsInput
@@ -69,7 +69,7 @@ const transportInputHandler = (category, inputValue) => {
         label="Time of arrival"
         placeholder="Enter your time of arrival"
         category="timeArrival"
-        
+        type="time"
         updateDataHandler={transportInputHandler}
       />
     </div>
