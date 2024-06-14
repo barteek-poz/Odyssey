d1fbdab516e484dcf8bb190aafa41e0aa1b4b830
barteek-poz <szowior4@wp.pl>
2024-06-14 20:47:17 +0200

    dynamic map display on mobile

--- src/pages/TravelDetails.jsx
+++ src/pages/TravelDetails.jsx
@@ -11,32 +11,32 @@ import ModalComponent from "../components/ModalComponent";
 
 const TravelDetails = () => {
   const loaderData = useLoaderData();
-  
+
   return (
-    <section className="TRAVEL-DETAILS w-full h-full">
+    <section className="TRAVEL-DETAILS w-full h-full mt-14 sm:mt-0">
       <Navigation />
       <div className="MAP w-full h-72 2xl:h-96">
         <Map singleTravelLocation={loaderData.geolocation} scrollZoom={false} />
       </div>
       <CenterComponent>
-          <div className="TRAVEL-DATA flex flex-col lg:flex-row justify-center">
-            <div className="LEFT-COLUM w-full lg:w-1/2 lg:py-10 pt-10 lg:px-10  flex flex-col items-center lg:items-start gap-8">
-              <div className="flex uppercase">
-                <h1 className="underline uppercase">Location:</h1>
-                <span className="pl-4">{loaderData.location}</span>
-              </div>
-              <div className="flex items-center gap-4 uppercase">
-                <span className="underline uppercase">Date:</span>
-                <DatePickerComponent />
-              </div>
-              <AccomodationDetails />
-              <Schedule />
+        <div className="TRAVEL-DATA flex flex-col lg:flex-row justify-center">
+          <div className="LEFT-COLUM w-full lg:w-1/2 lg:py-10 pt-10 lg:px-10  flex flex-col items-center lg:items-start gap-8">
+            <div className="flex uppercase">
+              <h1 className="underline uppercase">Location:</h1>
+              <span className="pl-4">{loaderData.location}</span>
             </div>
-            <div className="RIGHT-COLUMN lg:w-1/2 py-10 lg:px-10 lg:ml-0 mb-8 lg:mb-0 flex flex-col items-center lg:items-end gap-8">
-              <TransportDetails />
+            <div className="flex items-center gap-4 uppercase">
+              <span className="underline uppercase">Date:</span>
+              <DatePickerComponent />
             </div>
+            <AccomodationDetails />
+            <Schedule />
           </div>
-          <PieChartForm />
+          <div className="RIGHT-COLUMN lg:w-1/2 py-10 lg:px-10 lg:ml-0 mb-8 lg:mb-0 flex flex-col items-center lg:items-end gap-8">
+            <TransportDetails />
+          </div>
+        </div>
+        <PieChartForm />
         <ModalComponent />
       </CenterComponent>
     </section>
