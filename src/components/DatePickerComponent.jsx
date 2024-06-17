958383cc5aa05463fe9c1f259a83e8c495be630c
barteek-poz <szowior4@wp.pl>
2024-06-17 22:09:09 +0200

    added check icon to datepicker

--- src/components/DatePickerComponent.jsx
+++ src/components/DatePickerComponent.jsx
@@ -5,6 +5,7 @@ const { RangePicker } = DatePicker;
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firestore";
 import { useState } from "react";
+import CHECK_ICON from "../assets/check.svg";
 
 const DatePickerComponent = () => {
   const { date } = useLoaderData();
@@ -12,10 +13,12 @@ const DatePickerComponent = () => {
     firstDate: date[0].slice(0, 10),
     secondDate: date[1].slice(0, 10),
   });
-
+  const [showCheck, setShowCheck] = useState(false);
   const params = useParams();
   const currentTravel = doc(db, "travels", params.id);
+
   const onCalendarChange = async (dates, dateStrings) => {
+    console.log("test");
     if (
       dateStrings[0].trim().length !== 0 &&
       dateStrings[1].trim().length !== 0
@@ -27,15 +30,26 @@ const DatePickerComponent = () => {
       setDates({ firstDate: dateStrings[0], secondDate: dateStrings[1] });
     } else return;
   };
+
+  const checkIconHandler = () => {
+    setShowCheck(true);
+    setTimeout(() => {
+      setShowCheck(false);
+    }, 1000);
+  };
   return (
-    <Space direction="vertical" size={12}>
-      {date !== null && (
-        <RangePicker
-          onCalendarChange={onCalendarChange}
-          defaultValue={[dayjs(dates.firstDate), dayjs(dates.secondDate)]}
-        />
-      )}
-    </Space>
+    <div className="DATE-PICKER flex gap-2">
+      <Space direction="vertical" size={12}>
+        {date !== null && (
+          <RangePicker
+            onCalendarChange={onCalendarChange}
+            defaultValue={[dayjs(dates.firstDate), dayjs(dates.secondDate)]}
+            onBlur={checkIconHandler}
+          />
+        )}
+      </Space>
+     {showCheck&& <img src={CHECK_ICON} />}
+    </div>
   );
 };
 export default DatePickerComponent;
